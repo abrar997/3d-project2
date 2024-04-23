9fcdb264a117ca143bb41161c3c1b9cf67869686
abrar997 <abraralrawi997.com>
2024-04-23 05:00:58 +0300

    change image

--- src/app/components/events.tsx
+++ src/app/components/events.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Text from "./reusable/Text";
 
 export default function Events() {
@@ -7,7 +8,8 @@ export default function Events() {
       id="events"
     >
       <div className="col-span-2">
-        <img
+        <Image
+          alt=""
           src="https://images.unsplash.com/photo-1599564576463-d038c6945bb5?q=80&w=1767&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           className="lg:h-[420px] rounded lg:rounded-tr-full lg:rounded-br-xl border-2 lg:border border-secondary brightness-75 p-4"
           title=""
